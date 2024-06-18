d78830a21301f6fd4ba76ffc9be59896c0fecc2d
Francis Boafo <97594225+Franxbj@users.noreply.github.com>
2024-06-18 16:09:23 +0300

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,105 @@
-import { StyleSheet } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  TextInput,
+} from "react-native";
+import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import { useHeaderHeight } from "@react-navigation/elements";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
+const Page = () => {
+  const headerHeight = useHeaderHeight();
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => {}} style={{ marginLeft: 20 }}>
+              <Image
+                source={{
+                  uri: "https://xsgames.co/randomusers/avatar.php?g=female",
+                }}
+                style={{ width: 40, height: 40, borderRadius: 10 }}
+              />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                marginRight: 20,
+                backgroundColor: Colors.white,
+                padding: 10,
+                borderRadius: 10,
+                shadowColor: "#171717",
+                shadowOffset: { width: 2, height: 4 },
+                shadowOpacity: 0.2,
+                shadowRadius: 3,
+              }}
+            >
+              <Ionicons name="notifications" size={20} color={Colors.black} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <View style={[styles.container, { paddingTop: headerHeight }]}>
+        <Text style={styles.headingText}>Explore The Beautiful World!</Text>
+
+        <View style={styles.searchSectionWrapper}>
+          <View style={styles.searchBar}>
+            <Ionicons
+              name="search"
+              size={18}
+              style={{ marginRight: 5 }}
+              color={Colors.black}
+            />
+            <TextInput placeholder="search..." />
+          </View>
+          <TouchableOpacity onPress={() => {}} style={styles.filterBtn}>
+            <Ionicons name="options" size={28} color={Colors.white}/>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
   );
-}
+};
+
+export default Page;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: Colors.bgColor,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  headingText: {
+    fontSize: 28,
+    fontWeight: "800",
+    color: Colors.black,
+    marginTop: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  searchSectionWrapper: {
+    flexDirection: "row",
+    marginVertical: 20,
+  },
+  searchBar: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: Colors.white,
+    padding: 16,
+    borderRadius: 10,
+  },
+  filterBtn: {
+    backgroundColor: Colors.primaryColor,
+    padding: 12,
+    borderRadius: 10,
+    marginLeft: 20,
   },
 });
---
+++ constants/Colors.tsx
@@ -0,0 +1,8 @@
+export default {
+    primaryColor: '#ff7f36',
+    blue: '#0047AB',
+    bgColor: '#f4f4f4',
+    black: '#27283a',
+    white: '#FFFFFF',
+}
+
